a9aa17115d50a6215c95458e5407ee24b4bafca8
Seth Shelnutt <shelnutt2@gmail.com>
2012-08-17 21:11:55 -0400

    Valgrind: Added armv7 check for building. Build fails on armv6 due to armv7 only asm functions. Until armv6 supprot is merged upstream[1] conditionally building seems the best option.
    [1] https://bugs.kde.org/show_bug.cgi?id=276897
    
    Change-Id: I134b268b195e5277e589d87fba5e50d283a79aca

--- Android.mk
+++ Android.mk
@@ -1,1 +1,7 @@
-include $(call all-subdir-makefiles)
+ifeq ($(TARGET_ARCH),arm)
+ifeq ($(ARCH_ARM_HAVE_ARMV7A),true)
+    include $(call all-subdir-makefiles)
+endif
+else
+    include $(call all-subdir-makefiles)
+endif
