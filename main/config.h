981f7543d33c6e47e8844aac998abe6793fc0e1e
Ben Cheng <bccheng@google.com>
2013-02-01 10:51:03 -0800

    Add original config.h from 3.8.1
    Change-Id: Idc0648d3bbc3edec79d6b8b4be6daff3f5611ba0

--- main/config.h
+++ main/config.h
@@ -1,7 +1,8 @@
+/* config.h.  Generated from config.h.in by configure.  */
 /* config.h.in.  Generated from configure.in by autoheader.  */
 
-/* Define to 1 if you're using AIX 5.1 or 5.2 or 5.3 */
-/* #undef AIX5_LIBC */
+/* Define to 1 if you're using Bionic */
+/* #undef BIONIC_LIBC */
 
 /* DARWIN_VERS value for Mac OS X 10.5 */
 /* #undef DARWIN_10_5 */
@@ -11,6 +12,9 @@
 
 /* DARWIN_VERS value for Mac OS X 10.7 */
 /* #undef DARWIN_10_7 */
+
+/* DARWIN_VERS value for Mac OS X 10.8 */
+/* #undef DARWIN_10_8 */
 
 /* Define to 1 if you're using Darwin */
 /* #undef DARWIN_LIBC */
@@ -22,16 +26,28 @@
 /* #undef ENABLE_INNER */
 
 /* path to GDB */
-#define GDB_PATH "/system/bin/gdb"
+#define GDB_PATH "/usr/bin/gdb"
 
 /* Define to 1 if you're using glibc 2.10.x */
 /* #undef GLIBC_2_10 */
 
 /* Define to 1 if you're using glibc 2.11.x */
-/* #undef GLIBC_2_11 */
+#define GLIBC_2_11 1
 
 /* Define to 1 if you're using glibc 2.12.x */
 /* #undef GLIBC_2_12 */
+
+/* Define to 1 if you're using glibc 2.13.x */
+/* #undef GLIBC_2_13 */
+
+/* Define to 1 if you're using glibc 2.14.x */
+/* #undef GLIBC_2_14 */
+
+/* Define to 1 if you're using glibc 2.15.x */
+/* #undef GLIBC_2_15 */
+
+/* Define to 1 if you're using glibc 2.16.x */
+/* #undef GLIBC_2_16 */
 
 /* Define to 1 if you're using glibc 2.2.x */
 /* #undef GLIBC_2_2 */
@@ -66,8 +82,13 @@
 /* Define to 1 if as supports mtocrf/mfocrf. */
 /* #undef HAVE_AS_PPC_MFTOCRF */
 
-/* Define to 1 if gcc supports __sync_bool_compare_and_swap() a.o. */
-/* #undef HAVE_BUILTIN_ATOMIC */
+/* Define to 1 if gcc supports __sync_bool_compare_and_swap() and
+   __sync_add_and_fetch() for the primary target */
+#define HAVE_BUILTIN_ATOMIC 1
+
+/* Define to 1 if g++ supports __sync_bool_compare_and_swap() and
+   __sync_add_and_fetch() */
+#define HAVE_BUILTIN_ATOMIC_CXX 1
 
 /* Define to 1 if you have the `clock_gettime' function. */
 #define HAVE_CLOCK_GETTIME 1
@@ -76,22 +97,19 @@
 #define HAVE_CLOCK_MONOTONIC 1
 
 /* Define to 1 if you have the <endian.h> header file. */
-/* #define HAVE_ENDIAN_H 1 -- already defined in AndroidConfig.h */
+#define HAVE_ENDIAN_H 1
 
 /* Define to 1 if you have the `epoll_create' function. */
 #define HAVE_EPOLL_CREATE 1
 
 /* Define to 1 if you have the `epoll_pwait' function. */
-/* #undef HAVE_EPOLL_PWAIT */
+#define HAVE_EPOLL_PWAIT 1
 
 /* Define to 1 if you have the `eventfd' function. */
 #define HAVE_EVENTFD 1
 
 /* Define to 1 if you have the `eventfd_read' function. */
 #define HAVE_EVENTFD_READ 1
-
-/* Define to 1 if you have the `floor' function. */
-#define HAVE_FLOOR 1
 
 /* Define to 1 if you have the `getpagesize' function. */
 #define HAVE_GETPAGESIZE 1
@@ -102,8 +120,11 @@
 /* Define to 1 if you have the `klogctl' function. */
 #define HAVE_KLOGCTL 1
 
+/* Define to 1 if you have the `pthread' library (-lpthread). */
+#define HAVE_LIBPTHREAD 1
+
 /* Define to 1 if you have the `rt' library (-lrt). */
-/* #undef HAVE_LIBRT */
+#define HAVE_LIBRT 1
 
 /* Define to 1 if you have the `mallinfo' function. */
 #define HAVE_MALLINFO 1
@@ -124,25 +145,31 @@
 #define HAVE_MMAP 1
 
 /* Define to 1 if you have the <mqueue.h> header file. */
-/* #undef HAVE_MQUEUE_H */
+#define HAVE_MQUEUE_H 1
 
 /* Define to 1 if you have the `mremap' function. */
 #define HAVE_MREMAP 1
 
 /* Define to 1 if you have the `ppoll' function. */
-/* #undef HAVE_PPOLL */
+#define HAVE_PPOLL 1
+
+/* Define to 1 if you have the `process_vm_readv' function. */
+/* #undef HAVE_PROCESS_VM_READV */
+
+/* Define to 1 if you have the `process_vm_writev' function. */
+/* #undef HAVE_PROCESS_VM_WRITEV */
 
 /* Define to 1 if you have the `pthread_barrier_init' function. */
-/* #undef HAVE_PTHREAD_BARRIER_INIT */
+#define HAVE_PTHREAD_BARRIER_INIT 1
 
 /* Define to 1 if you have the `pthread_condattr_setclock' function. */
-/* #undef HAVE_PTHREAD_CONDATTR_SETCLOCK */
+#define HAVE_PTHREAD_CONDATTR_SETCLOCK 1
 
 /* Define to 1 if you have the `pthread_create@glibc2.0' function. */
 /* #undef HAVE_PTHREAD_CREATE_GLIBC_2_0 */
 
 /* Define to 1 if you have the `PTHREAD_MUTEX_ADAPTIVE_NP' constant. */
-/* #undef HAVE_PTHREAD_MUTEX_ADAPTIVE_NP */
+#define HAVE_PTHREAD_MUTEX_ADAPTIVE_NP 1
 
 /* Define to 1 if you have the `PTHREAD_MUTEX_ERRORCHECK_NP' constant. */
 #define HAVE_PTHREAD_MUTEX_ERRORCHECK_NP 1
@@ -154,7 +181,7 @@
 #define HAVE_PTHREAD_MUTEX_TIMEDLOCK 1
 
 /* Define to 1 if pthread_mutex_t has a member __data.__kind. */
-/* #undef HAVE_PTHREAD_MUTEX_T__DATA__KIND */
+#define HAVE_PTHREAD_MUTEX_T__DATA__KIND 1
 
 /* Define to 1 if pthread_mutex_t has a member called __m_kind. */
 /* #undef HAVE_PTHREAD_MUTEX_T__M_KIND */
@@ -163,6 +190,9 @@
    constant. */
 #define HAVE_PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP 1
 
+/* Define to 1 if you have the `pthread_rwlock_t' type. */
+#define HAVE_PTHREAD_RWLOCK_T 1
+
 /* Define to 1 if you have the `pthread_rwlock_timedrdlock' function. */
 #define HAVE_PTHREAD_RWLOCK_TIMEDRDLOCK 1
 
@@ -170,29 +200,25 @@
 #define HAVE_PTHREAD_RWLOCK_TIMEDWRLOCK 1
 
 /* Define to 1 if you have the `pthread_spin_lock' function. */
-/* #undef HAVE_PTHREAD_SPIN_LOCK */
+#define HAVE_PTHREAD_SPIN_LOCK 1
 
 /* Define to 1 if you have the `pthread_yield' function. */
-/* #undef HAVE_PTHREAD_YIELD */
-
-/* Define to 1 if the installed version of Qt4 provides QAtomicInt. */
-/* #undef HAVE_QTCORE_QATOMICINT */
-
-/* Define to 1 if the installed version of Qt4 provides QMutex::tryLock(int).
-   */
-/* #undef HAVE_QTCORE_QMUTEX_TRYLOCK_INT */
+#define HAVE_PTHREAD_YIELD 1
 
 /* Define to 1 if you have the `readlinkat' function. */
-/* #undef HAVE_READLINKAT */
+#define HAVE_READLINKAT 1
 
 /* Define to 1 if you have the `semtimedop' function. */
-/* #undef HAVE_SEMTIMEDOP */
+#define HAVE_SEMTIMEDOP 1
+
+/* Define to 1 if libstd++ supports annotating shared pointers */
+/* #undef HAVE_SHARED_POINTER_ANNOTATION */
 
 /* Define to 1 if you have the `signalfd' function. */
-/* #undef HAVE_SIGNALFD */
+#define HAVE_SIGNALFD 1
 
 /* Define to 1 if you have the `sigwaitinfo' function. */
-/* #undef HAVE_SIGWAITINFO */
+#define HAVE_SIGWAITINFO 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
@@ -225,7 +251,7 @@
 #define HAVE_SYSCALL 1
 
 /* Define to 1 if you have the <sys/endian.h> header file. */
-#define HAVE_SYS_ENDIAN_H 1
+/* #undef HAVE_SYS_ENDIAN_H */
 
 /* Define to 1 if you have the <sys/epoll.h> header file. */
 #define HAVE_SYS_EPOLL_H 1
@@ -243,7 +269,7 @@
 #define HAVE_SYS_POLL_H 1
 
 /* Define to 1 if you have the <sys/signalfd.h> header file. */
-/* #undef HAVE_SYS_SIGNALFD_H */
+#define HAVE_SYS_SIGNALFD_H 1
 
 /* Define to 1 if you have the <sys/signal.h> header file. */
 #define HAVE_SYS_SIGNAL_H 1
@@ -260,23 +286,26 @@
 /* Define to 1 if you have the <sys/types.h> header file. */
 #define HAVE_SYS_TYPES_H 1
 
-/* Define to 1 if you have the `timerfd' function. */
-/* #undef HAVE_TIMERFD */
-
 /* can use __thread to define thread-local variables */
-/* #undef HAVE_TLS */
+#define HAVE_TLS 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
 
+/* Define to 1 if you have a usable <linux/futex.h> header file. */
+#define HAVE_USABLE_LINUX_FUTEX_H 1
+
 /* Define to 1 if you have the `utimensat' function. */
-/* #undef HAVE_UTIMENSAT */
+#define HAVE_UTIMENSAT 1
 
 /* Define to 1 if you're using Linux 2.4.x */
 /* #undef KERNEL_2_4 */
 
-/* Define to 1 if you're using Linux 2.6.x */
+/* Define to 1 if you're using Linux 2.6.x or Linux 3.x */
 #define KERNEL_2_6 1
+
+/* configured default page size 4k */
+#define MIPS_PAGE_SHIFT 12
 
 /* Define to 1 if your C compiler doesn't accept -c and -o together. */
 /* #undef NO_MINUS_C_MINUS_O */
@@ -291,7 +320,7 @@
 #define PACKAGE_NAME "Valgrind"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Valgrind 3.6.1"
+#define PACKAGE_STRING "Valgrind 3.8.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "valgrind"
@@ -300,10 +329,10 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.6.1"
+#define PACKAGE_VERSION "3.8.1"
 
-/* Define as the return type of signal handlers (`int' or `void'). */
-#define RETSIGTYPE void
+/* configured memory alignment 2*PAGE_SIZE */
+#define SHM_ALIGNMENT 2*(1UL << MIPS_PAGE_SHIFT)
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
@@ -312,10 +341,10 @@
 #define TIME_WITH_SYS_TIME 1
 
 /* Version number of package */
-#define VERSION "3.6.1"
+#define VERSION "3.8.1"
 
 /* Temporary files directory */
-#define VG_TMPDIR "/data/local/tmp"
+#define VG_TMPDIR "/tmp"
 
 /* Define to `int' if <sys/types.h> doesn't define. */
 /* #undef gid_t */
@@ -328,12 +357,3 @@
 
 /* Define to `int' if <sys/types.h> doesn't define. */
 /* #undef uid_t */
-
-/* path to egrep */
-#define EGREP_PATH "/system/bin/grep -E"
-
-/* path to sh */
-#define SH_PATH "/system/bin/sh"
-
-/* path to strings */
-#define STRINGS_PATH "/system/bin/strings"
