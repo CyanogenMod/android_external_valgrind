34694b13aa9dec6d310a4c551155859780dfade4
Dmitriy Ivanov <dimitry@google.com>
2014-12-18 15:05:37 -0800

    Revert "set/get_gs hack similar to set/get_fs hack"
    This reverts commit e95725df03d54030065063e5ef9614eba368c5d2.
    
    Bug: 18597541
    Change-Id: I3233bd68743ae86cd98b1a97643992a0480fed7d

--- main/VEX/pub/libvex.h
+++ main/VEX/pub/libvex.h
@@ -316,14 +316,14 @@ void LibVEX_default_VexArchInfo ( /*OUT*/VexArchInfo* vai );
       guest is amd64-linux                ==> 128
       guest is other                      ==> inapplicable
 
-   guest_amd64_assume_fs_is_const
+   guest_amd64_assume_fs_is_zero
       guest is amd64-linux                ==> True
       guest is amd64-darwin               ==> False
       guest is other                      ==> inapplicable
 
-   guest_amd64_assume_gs_is_const
+   guest_amd64_assume_gs_is_0x60
       guest is amd64-darwin               ==> True
-      guest is amd64-linux                ==> True
+      guest is amd64-linux                ==> False
       guest is other                      ==> inapplicable
 
    guest_ppc_zap_RZ_at_blr
@@ -350,13 +350,13 @@ typedef
 
       /* AMD64 GUESTS only: should we translate %fs-prefixed
          instructions using the assumption that %fs always contains
-         the same value? (typically zero on linux) */
-      Bool guest_amd64_assume_fs_is_const;
+         zero? */
+      Bool guest_amd64_assume_fs_is_zero;
 
       /* AMD64 GUESTS only: should we translate %gs-prefixed
          instructions using the assumption that %gs always contains
-         the same value? (typically 0x60 on darwin)? */
-      Bool guest_amd64_assume_gs_is_const;
+         0x60? */
+      Bool guest_amd64_assume_gs_is_0x60;
 
       /* PPC GUESTS only: should we zap the stack red zone at a 'blr'
          (function return) ? */
